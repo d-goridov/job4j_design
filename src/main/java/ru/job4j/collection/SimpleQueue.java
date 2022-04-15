94e949617ab35242123f80231f7ebcb65edb2c08
d.goridov <dimagoridov@yandex.ru>
2022-04-15 12:26:25 +0300

    5. Очередь на двух стеках [#160] Сделал переменную size с модификатором private

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -4,8 +4,8 @@ package ru.job4j.collection;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    int sizeIn = 0;
-    int sizeOut = 0;
+    private int sizeIn = 0;
+    private int sizeOut = 0;
 
     public T poll() {
         if (sizeOut == 0) {
