8c6a3b7011228e33ec1b289755b64a4d5d9e9afc
d.goridov <dimagoridov@yandex.ru>
2022-04-24 13:25:06 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/io/Matrix.java
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Matrix {
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ public class Matrix {
                 }
                 out.write(System.lineSeparator().getBytes());
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
