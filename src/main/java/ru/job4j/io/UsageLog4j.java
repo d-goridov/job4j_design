e141515baf7aaa6ad448dba3bdf7deab0e87f39e
d.goridov <dimagoridov@yandex.ru>
2022-05-06 01:26:20 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,23 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte age = 3;
+        short engineCil = 4;
+        int power = 101;
+        float weight = 212.32f;
+        double distance = 15004.54;
+        long numberLot = 32456724L;
+        boolean isCrushed = false;
+        char color = 'R';
+
+        LOG.debug("Motorcycle Info age: {}", age);
+        LOG.debug("Motorcycle Info engineCil: {}", engineCil);
+        LOG.debug("Motorcycle Info power: {}", power);
+        LOG.debug("Motorcycle Info weight: {}", weight);
+        LOG.debug("Motorcycle Info distance: {}", distance);
+        LOG.debug("Motorcycle Info numberLot: {}", numberLot);
+        LOG.debug("Motorcycle Info isCrushed: {}", isCrushed);
+        LOG.debug("Motorcycle Info color: {}", color);
+
     }
 }
