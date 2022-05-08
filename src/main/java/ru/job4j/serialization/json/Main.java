48157c8419189d60f3ee0a36c6c667fe44ef9212
d.goridov <dimagoridov@yandex.ru>
2022-05-08 23:07:53 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/Main.java
+++ src/main/java/ru/job4j/serialization/json/Main.java
@@ -2,6 +2,11 @@ package ru.job4j.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,5 +32,22 @@ public class Main {
 
         final Client clientMod = gson.fromJson(clientJson, Client.class);
         System.out.println(clientMod);
+
+        JSONObject jsonPassport = new JSONObject("{\"number\":\"32098\"}");
+        List<String> list = new ArrayList<>();
+        list.add("Portugal");
+        list.add("New Zealand");
+        list.add("Malaysia");
+        JSONArray jsonWishList = new JSONArray(list);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", client.getName());
+        jsonObject.put("age", client.getAge());
+        jsonObject.put("hasVisa", client.isHasVisa());
+        jsonObject.put("wishList", jsonWishList);
+        jsonObject.put("passport", jsonPassport);
+
+        System.out.println(jsonObject);
+        System.out.println(new JSONObject(client));
     }
 }
