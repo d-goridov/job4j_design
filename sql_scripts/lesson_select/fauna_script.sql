5798c7a0bcb2b22ac1fef6c0467546f5234980dd
d.goridov <dimagoridov@yandex.ru>
2022-05-18 16:58:41 +0300

    0. Select [#6861]

--- sql_scripts/lesson_select/fauna_script.sql
+++ sql_scripts/lesson_select/fauna_script.sql
@@ -6,19 +6,19 @@ CREATE table fauna (
 );
 
 insert into fauna(name, avg_age, discovery_date)
-values (fish_shark, 12908, date '1934-09-05');
+values ('fish_shark', 12908, date '1934-09-05');
 
 insert into fauna(name, avg_age, discovery_date)
-values (animal_bear, 15447, date '1921-07-10');
+values ('animal_bear', 15447, date '1921-07-10');
 
 insert into fauna(name, avg_age, discovery_date)
-values (bird_eagle, 25000, date '1951-12-01');
+values ('bird_eagle', 25000, date '1951-12-01');
 
 insert into fauna(name, avg_age, discovery_date)
-values (shake_viper, 9005, date '1960-07-12');
+values ('shake_viper', 9005, date '1960-07-12');
 
 insert into fauna(name, avg_age, discovery_date)
-values (arthropods_spider, 5498, null);
+values ('arthropods_spider', 5498, null);
 
 select * from fauna where name LIKE '%fish%';
 select * from fauna where avg_age between 10000 and 21000;
