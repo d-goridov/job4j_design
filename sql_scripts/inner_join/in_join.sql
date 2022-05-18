0184f574874d6564c87c44b51ad962481040a2a5
d.goridov <dimagoridov@yandex.ru>
2022-05-18 23:57:49 +0300

    1. Inner join [#373617]

---
+++ sql_scripts/inner_join/in_join.sql
@@ -0,0 +1,32 @@
+create table faculty(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table students(
+    id serial primary key,
+    name varchar(255),
+    faculty_id int references faculty(id)
+);
+
+insert into faculty(name) values ('IT');
+insert into faculty(name) values ('ECONOMIC');
+insert into faculty(name) values ('BUILDING');
+
+insert into students(name, faculty_id) values('Oleg', 1);
+insert into students(name, faculty_id) values('Maria', 2);
+insert into students(name, faculty_id) values('Dmitriy', 1);
+insert into students(name, faculty_id) values ('Olga', 2);
+insert into students(name, faculty_id) values ('Petr', 3);
+
+select st.id as id, st.name as student, fac.name as faculty
+from students as st join faculty as fac on (st.faculty_id = fac.id)
+order by st.id;
+
+select st.id as id, st.name as student, fac.id as id_fac, fac.name as faculty
+from students as st join faculty as fac on (st.id = fac.id)
+order by st.name;
+
+select st.id as id, st.name as student, fac.name as faculty
+from students as st join faculty as fac on (st.faculty_id = fac.id)
+where fac.name = 'ECONOMIC';
