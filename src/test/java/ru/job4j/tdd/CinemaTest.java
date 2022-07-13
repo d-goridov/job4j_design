30b332c8a140b8c37073967162f0fc9e9ae52840
d.goridov <dimagoridov@yandex.ru>
2022-07-13 23:44:09 +0300

    2. Что такое TDD? [#4918 #321828]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.tdd;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.*;
@@ -8,7 +9,6 @@ import java.util.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertNull;
 
 
 public class CinemaTest {
@@ -25,39 +25,44 @@ public class CinemaTest {
     public void whenBuy() {
         Account account = new AccountCinema();
         Calendar date = Calendar.getInstance();
-        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
+        date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-        assertThat(ticket, is(ticket));
-    }
-
-    @Test
-    public void whenCantBuy() {
-        Account account = new AccountCinema();
-        Calendar date = Calendar.getInstance();
-        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
-        Ticket ticket = cinema.buy(account, 1, 1, date);
-        assertNull(ticket);
+        assertThat(ticket, is(nullValue()));
     }
 
     @Test
     public void whenFind() {
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
-        assertThat(sessions, is(sessions));
-    }
-
-    @Test
-    public void whenNotFind() {
-        cinema.add(new Session3D());
-        List<Session> sessions = cinema.find(session -> false);
         assertThat(sessions, is(nullValue()));
     }
 
-    @Test
-    public void whenAddAndCount() {
-        cinema.add(new Session3D());
-        cinema.add(new Session3D());
-        List<Session> sessions = List.of(new Session3D(), new Session3D());
-        assertThat(sessions.size(), is(2));
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenInvalidPlace() {
+        Account account = new AccountCinema();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, -10, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenInvalidDate() {
+        Account account = new AccountCinema();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 12, 43, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuySoldPlace() {
+        Account account = new AccountCinema();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
     }
 }
